19dcfbd63e8cce32c9cba62e44cbb85ba55ccea7
acschares <32584155+acschares@users.noreply.github.com>
2017-12-12 20:42:22 -0800

    Added new pages, bootstrap, base JS logic

--- js/final.js
+++ js/final.js
@@ -1,1 +1,15 @@
+$(document).ready(function(){
+  $('#contact-submit').click(function(event){
+    event.preventDefault();
 
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var message = $('#message').val();
+
+    alert(name);
+    alert(email);
+    alert(message);
+
+    alert("Your email was sent");
+  })
+});
