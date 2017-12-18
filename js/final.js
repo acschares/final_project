639990e1be5911e12d7a1907384516ec2a142553
acschares <32584155+acschares@users.noreply.github.com>
2017-12-17 17:17:02 -0800

    Finished index.html

--- js/final.js
+++ js/final.js
@@ -14,6 +14,8 @@ $(document).ready(function(){
 
     function onSuccess() {
         alert("Email sent");
+        $("#jquery_form" + " [name='subject']").val('');
+        $("#jquery_form" + " [name='text']").val('');
     }
 
     function onError(error) {
