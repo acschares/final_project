e281a84ad0056c10b0901f8af965ff2da557e63a
acschares <32584155+acschares@users.noreply.github.com>
2017-12-18 17:53:29 -0800

    Changed files around to use better selectors, added comments

--- js/final.js
+++ js/final.js
@@ -1,33 +1,40 @@
 $(document).ready(function(){
-  $("#jquery_form").submit(function(event){
-    event.preventDefault();
+  $("#jquery_form").submit(function(event){  //run this when submit button is clicked
+    event.preventDefault(); //prevent default action
 
+    //build data dictionary for API call to Postmail
     var data = {
         "access_token": "y2jgnb2hqeitcko25k5d3z2a"
     };
 	
-    var subject = $("#jquery_form" + " [name='subject']").val();
-    var message = $("#jquery_form" + " [name='text']").val();
+    //get subject and message values from form
+    var subject = $("#subject").val();
+    var message = $("#message").val();
 
+    //add subject and message values to dictionary
     data['subject'] = subject;
     data['text'] = message;
 
+    //define success function: alert user email was sent, clear out the form fields
     function onSuccess() {
         alert("Email sent");
-        $("#jquery_form" + " [name='subject']").val('');
-        $("#jquery_form" + " [name='text']").val('');
+        $("#subject").val('');
+        $("#message").val('');
     }
 
+    //define error function: alert user that email failed to send
     function onError(error) {
         alert("Email failed to send");
     }
 
+    //send data to Postmail to send email, run either onSuccess or onError
     $.post('https://postmail.invotes.com/send',
         data,
         onSuccess
     ).fail(onError);
   })
 
+  //Scroll down to portfolio section if arrow is selected
   $('#portfolio-link').click(function(event){
     event.preventDefault();
     var tag = $("#portfolio");
